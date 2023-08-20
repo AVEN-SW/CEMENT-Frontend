1b49b70be2dcea97c3a7a0166715a5e135d4baa1
kimcurly <sanbonai0728@gmail.com>
2023-08-21 00:05:19 +0900

    feat: 스크롤을 감지하여 Header의 스타일링 변경 기능 추가

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,17 +1,26 @@
+import { useState, useEffect } from 'react'
 import { styled } from "styled-components"
 
 const Header = () => {
+    const [scrollLocation, setScrollLocation] = useState(0);
+    const updateScroll = () => {
+        setScrollLocation(window.scrollY || document.documentElement.scrollTop);
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll', updateScroll);
+    });
+
   return (
-    <HeaderSection>
+    <HeaderSection scroll={scrollLocation}>
         <FlexContainer>
             <LogoSection>
                 <img src="./src/assets/logo.png" alt="로고 이미지" />
             </LogoSection>
             <NavigationSection>
-                <NavigationButton>Culture</NavigationButton>
-                <NavigationButton>People</NavigationButton>
-                <NavigationButton>Benefits</NavigationButton>
-                <NavigationButton>Portfolio</NavigationButton>
+                <NavigationButton scroll={scrollLocation}>Culture</NavigationButton>
+                <NavigationButton scroll={scrollLocation}>People</NavigationButton>
+                <NavigationButton scroll={scrollLocation}>Benefits</NavigationButton>
+                <NavigationButton scroll={scrollLocation}>Portfolio</NavigationButton>
             </NavigationSection>
         </FlexContainer>
     </HeaderSection>
@@ -20,13 +29,15 @@ const Header = () => {
 
 export default Header
 
-const HeaderSection = styled.div`
+const HeaderSection = styled.div<{scroll: number}>`
     position: fixed;
     top: 0;
     width: 100%;
     height: 117px;
+    background: ${props => props.scroll > 117 && "#fff"};
+    transition: all ease 0.3s;
     
-    /* box-shadow: 0px 8px 7px 0px rgba(0, 0, 0, 0.13); */
+    box-shadow: ${props => props.scroll > 117 && "0px 8px 7px 0px rgba(0, 0, 0, 0.13)"};
 `
 
 const FlexContainer = styled.div`
@@ -38,9 +49,7 @@ const FlexContainer = styled.div`
     height: 100%;
 `
 
-const LogoSection = styled.div`
-    
-`
+const LogoSection = styled.div``
 
 const NavigationSection = styled.div`
     display: flex;
@@ -50,12 +59,12 @@ const NavigationSection = styled.div`
     width: 663px;
 `
 
-const NavigationButton = styled.button`
+const NavigationButton = styled.button<{scroll: number}>`
     background: unset;
     border: unset;
+    transition: all ease 0.3s;
 
     font-size: 28px;
     font-weight: 400;
-    /* color: #5B5B5B; */
-    color: #fff;
+    color: ${props => props.scroll > 117 ? "#5B5B5B" : "#fff"}
 `
