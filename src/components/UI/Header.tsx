6c5d3f56a20eef4fa3bfc2645322785a4476f77c
kimcurly <sanbonai0728@gmail.com>
2023-08-20 22:24:48 +0900

    feat: Header 컴포넌트 추가

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,7 +1,59 @@
+import { styled } from "styled-components"
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <HeaderSection>
+        <FlexContainer>
+            <LogoSection>
+                <img src="./src/assets/logo.png" alt="로고 이미지" />
+            </LogoSection>
+            <NavigationSection>
+                <NavigationButton>Culture</NavigationButton>
+                <NavigationButton>People</NavigationButton>
+                <NavigationButton>Benefits</NavigationButton>
+                <NavigationButton>Portfolio</NavigationButton>
+            </NavigationSection>
+        </FlexContainer>
+    </HeaderSection>
   )
 }
 
 export default Header
+
+const HeaderSection = styled.div`
+    width: 100%;
+    height: 117px;
+    
+    background: #FFF;
+    box-shadow: 0px 8px 7px 0px rgba(0, 0, 0, 0.13);
+`
+
+const FlexContainer = styled.div`
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    width: 100%;
+    height: 100%;
+`
+
+const LogoSection = styled.div`
+    
+`
+
+const NavigationSection = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    width: 663px;
+`
+
+const NavigationButton = styled.button`
+    background: unset;
+    border: unset;
+
+    font-size: 28px;
+    font-weight: 400;
+    color: #5B5B5B;
+`
