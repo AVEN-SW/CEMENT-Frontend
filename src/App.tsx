9afc271058f30beec8b84ad5a1dc9f3a7d698c93
kimcurly <sanbonai0728@gmail.com>
2023-08-20 22:20:56 +0900

    design: 전역 스타일링 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom'
 import Header from './components/UI/Header'
 import './App.css'
+import GlobalStyle from './style/GlobalStyle'
 
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Outlet />
     </>
