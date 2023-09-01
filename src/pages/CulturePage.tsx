6e0b9dcbab9fb8d8fdaa6e6b52674971c60bf0ee
kimcurly <sanbonai0728@gmail.com>
2023-09-01 10:08:11 +0900

    feat: 첫 번째 갤러리 두 번째 사진 추가

--- src/pages/CulturePage.tsx
+++ src/pages/CulturePage.tsx
@@ -15,7 +15,14 @@ const CulturePage = () => {
               나이가 아니라 실력과 태도, 의지로 인정받습니다. <br />
               전문성과 영향력을 갖췄다면 누구나 일을 주도할 수 있습니다.
             </FirstGallerySubTitle>
-            <FirstGallery />
+            <FirstGalleryImage
+              src="src/assets/first-gallery.png"
+              alt="첫 번째 갤러리 이미지"
+            />
+            <FirstGallerySecondImage
+              src="src/assets/first-gallery2.png"
+              alt="첫 번째 갤러리 두 번쨰 이미지"
+            />
           </FirstGalleryContainer>
         </CultureGalleryContainer>
       </FlexContainer>
@@ -75,31 +82,45 @@ const FirstGalleryContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  align-items: center;
+  align-items: flex-start;
 
+  position: relative;
   width: 80%;
-  height: 870px;
+  height: 800px;
 `;
 
-const FirstGalleryTitle = styled.h2`
+const FirstGalleryTitle = styled.div`
+  /* display: flex;
+  align-items: flex-start;
+
+  width: 100%;
+  height: 100%; */
+
   color: #5b5b5b;
   font-family: NEXONFootballGothicBA1;
-  font-size: 64px;
+  font-size: 60px;
   font-weight: 500;
 `;
 
 const FirstGallerySubTitle = styled.div`
   color: #5b5b5b;
   font-family: NEXONFootballGothicBA1;
-  font-size: 40px;
+  font-size: 36px;
   font-weight: 300;
 `;
 
-const FirstGallery = styled.img`
-  width: 1000px;
-  min-height: 565px;
+const FirstGalleryImage = styled.img`
+  width: 60%;
 
-  background:
-    url('src/assets/first-gallery.png'),
-    lightgray 50% / cover no-repeat;
+  object-fit: cover;
+`;
+
+const FirstGallerySecondImage = styled.img`
+  position: absolute;
+  right: 0;
+  bottom: 0;
+
+  height: 800px;
+
+  object-fit: cover;
 `;
