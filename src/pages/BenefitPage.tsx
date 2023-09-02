70ea461c5f60b599d21d30de5f3c4dd1f2d8c482
kimcurly <sanbonai0728@gmail.com>
2023-09-02 17:05:22 +0900

    feat: Benefit 페이지 기본 세팅

--- src/pages/BenefitPage.tsx
+++ src/pages/BenefitPage.tsx
@@ -1,5 +1,52 @@
+import { styled } from 'styled-components';
+
 const BenefitPage = () => {
-  return <div>BenefitPage</div>;
+  return (
+    <BenefitPageSection>
+      <FlexContainer>
+        <BenefitTitleContainer>
+          <BenefitTitle>Benefits</BenefitTitle>
+        </BenefitTitleContainer>
+      </FlexContainer>
+    </BenefitPageSection>
+  );
 };
 
 export default BenefitPage;
+
+const BenefitPageSection = styled.div`
+  width: 100%;
+
+  padding-top: 117px;
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+  margin-top: 200px;
+`;
+
+const BenefitTitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 80%;
+  margin-bottom: 140px;
+`;
+
+const BenefitTitle = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+
+  color: #5b5b5b;
+  font-family: Gmarket Sans;
+  font-size: 80px;
+  font-weight: bold;
+`;
