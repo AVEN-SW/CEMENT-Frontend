c1b5dcbe1f4f593e5c43dcf36753a23187ea0fb0
kimcurly <sanbonai0728@gmail.com>
2023-09-07 01:58:57 +0900

    feat: Portfolio 부분 스타일링 추가

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -27,21 +27,22 @@ const MainPage = () => {
           <PortfolioTitle>PORTFOLIO</PortfolioTitle>
           <PortfolioImages>
             <PortfolioImage
-              src="./src/assets/sample.png"
+              src="./src/assets/main-sample.png"
               alt="포트폴리오 이미지"
             />
             <PortfolioImage
-              src="./src/assets/sample.png"
+              src="./src/assets/main-sample.png"
               alt="포트폴리오 이미지"
             />
             <PortfolioImage
-              src="./src/assets/sample.png"
+              src="./src/assets/main-sample.png"
               alt="포트폴리오 이미지"
             />
             <PortfolioImage
-              src="./src/assets/sample.png"
+              src="./src/assets/main-sample.png"
               alt="포트폴리오 이미지"
             />
+            <PortfolioSignature>Signature</PortfolioSignature>
           </PortfolioImages>
         </PortfolioContainer>
       </PortfolioSection>
@@ -52,9 +53,9 @@ const MainPage = () => {
       </ContactSection>
     </MainPageSeciton>
   );
-}
+};
 
-export default MainPage
+export default MainPage;
 
 const MainPageSeciton = styled.div`
   width: 100%;
@@ -64,110 +65,128 @@ const MainPageSeciton = styled.div`
 `;
 
 const FlexContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    width: 100%;
-    height: 100%;
-`
+  width: 100%;
+  height: 100%;
+`;
 
 const MainTitleSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const MainTitle = styled.div`
-    padding-bottom: 172px;
+  padding-bottom: 172px;
 
-    font-size: 123px;
-    font-weight: 400;
-    color: #FFF;
-`
+  font-size: 123px;
+  font-weight: 400;
+  color: #fff;
+`;
 
 const MainSubTitle = styled.div`
-    text-align: center;
-    font-family: NanumBarunGothicOTF;
-    font-size: 28px;
-    font-weight: 400;
-    color: #FFF;
-`
+  text-align: center;
+  font-family: NanumBarunGothicOTF;
+  font-size: 28px;
+  font-weight: 400;
+  color: #fff;
+`;
 
 const SloganSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
 
-    width: 100%;
-    height: 840px;
-    background: #FFF;
-`
+  width: 100%;
+  height: 840px;
+  background: #fff;
+`;
 
 const SloganTitle = styled.div`
-    color: #000;
-    font-size: 48px;
-    font-weight: 400;
-`
+  color: #000;
+  font-size: 48px;
+  font-weight: 400;
+`;
 
 const Slogan = styled.div`
-    background: linear-gradient(90deg, #FC5C7D 0%, #6A82FB 100%);
-    background-clip: text;
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    
-    font-family: NEXONFootballGothicBA1;
-    font-size: 90px;
-    font-weight: 300;
-`
+  background: linear-gradient(90deg, #fc5c7d 0%, #6a82fb 100%);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+
+  font-family: NEXONFootballGothicBA1;
+  font-size: 90px;
+  font-weight: 300;
+`;
 
 const SloganDescription = styled.div`
-    color: #000;
-    font-family: NanumBarunGothicOTF;
-    font-size: 40px;
-    font-weight: 400;
-`
+  color: #000;
+  font-family: NanumBarunGothicOTF;
+  font-size: 40px;
+  font-weight: 400;
+`;
 
 const PortfolioSection = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    width: 100%;
-    height: 968px;
-    background: #E4932B;
-`
+  width: 100%;
+  height: 968px;
+  background: #e4932b;
+`;
 
 const PortfolioContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 
-    width: 100%;
-    height: 731px;
-`
+  width: 100%;
+  height: 60%;
+`;
 
 const PortfolioTitle = styled.div`
-    color: #FFF;
-    font-size: 48px;
-    font-weight: 400;
-`
+  color: #fff;
+  font-size: 48px;
+  font-weight: 400;
+`;
 
 const PortfolioImages = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-    width: 90%;
-`
+  position: relative;
+  width: 70%;
+`;
 
 const PortfolioImage = styled.img`
-    width: 374px;
-    height: 536px;
-`
+  width: 20%;
+
+  object-fit: cover;
+`;
+
+const PortfolioSignature = styled.div`
+  position: absolute;
+  left: -100px;
+  top: -50px;
+  transform: rotate(-9.029deg);
+
+  color: #ff0f0f;
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+  font-family: 'Yellowtail', cursive;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
 
 const ContactSection = styled.div`
     width: 100%;
