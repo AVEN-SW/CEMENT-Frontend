bc7e3c5041b1a8d70b18d0a8aabd8bde4099dff3
kimcurly <sanbonai0728@gmail.com>
2023-09-07 17:53:02 +0900

    fix: SecondGallery 컴포넌트 리사이징

--- src/components/Culture/SecondGallery.tsx
+++ src/components/Culture/SecondGallery.tsx
@@ -4,19 +4,23 @@ const SecondGallery = () => {
   return (
     <SecondGalleryContainer>
       <SecondGalleryTitle>자율적으로, 책임감 있게</SecondGalleryTitle>
-      <SecondGallerySubTitle>
-        각자 집중할 수 있는 시간과 공간을 선택하여 일하며 <br />
-        자신이 맡은 일을 책임감 있게 판단하고 실행합니다. <br />
-        스스로의 기준을 만족시키고 뛰어넘는 데 집중합니다.
-      </SecondGallerySubTitle>
-      <SecondGalleryImage
-        src="src/assets/second-gallery.png"
-        alt="두 번째 갤러리 이미지"
-      />
-      <SecondGallerySecondImage
-        src="src/assets/second-gallery2.png"
-        alt="두 번째 갤러리 두 번째 이미지"
-      />
+      <SecondGalleryContents>
+        <SecondGallerySecondImage
+          src="src/assets/second-gallery2.png"
+          alt="두 번째 갤러리 두 번째 이미지"
+        />
+        <SecondGallerySubTitleContainer>
+          <SecondGallerySubTitle>
+            각자 집중할 수 있는 시간과 공간을 선택하여 일하며 <br />
+            자신이 맡은 일을 책임감 있게 판단하고 실행합니다. <br />
+            스스로의 기준을 만족시키고 뛰어넘는 데 집중합니다.
+          </SecondGallerySubTitle>
+          <SecondGalleryImage
+            src="src/assets/second-gallery.png"
+            alt="두 번째 갤러리 이미지"
+          />
+        </SecondGallerySubTitleContainer>
+      </SecondGalleryContents>
     </SecondGalleryContainer>
   );
 };
@@ -26,42 +30,60 @@ export default SecondGallery;
 const SecondGalleryContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
-  align-items: flex-end;
+  justify-content: space-around;
+  align-items: center;
 
-  position: relative;
-  width: 80%;
-  height: 830px;
-  z-index: -1;
+  width: 100%;
+  height: 750px;
 `;
 
 const SecondGalleryTitle = styled.div`
+  text-align: right;
+
+  width: 100%;
+
   color: #5b5b5b;
   font-family: NEXONFootballGothicBA1;
-  font-size: 60px;
+  font-size: 44px;
   font-weight: 500;
 `;
 
+const SecondGalleryContents = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  width: 100%;
+`;
+
+const SecondGallerySubTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-end;
+
+  width: 60%;
+`;
+
 const SecondGallerySubTitle = styled.div`
+  margin-bottom: 15%;
+
   color: #5b5b5b;
   font-family: NEXONFootballGothicBA1;
-  font-size: 36px;
+  font-size: 28px;
   font-weight: 300;
   text-align: right;
 `;
 
 const SecondGalleryImage = styled.img`
-  width: 60%;
+  width: 100%;
+  max-height: 50%;
 
   object-fit: cover;
 `;
 
 const SecondGallerySecondImage = styled.img`
-  position: absolute;
-  left: 0;
-  top: 0;
-
-  height: 450px;
+  max-height: 60%;
 
   object-fit: cover;
 `;
