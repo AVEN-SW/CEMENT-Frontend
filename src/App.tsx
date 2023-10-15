1af1f603b49c9cacd458ede0c5886fe6f7fbe37d
kimcurly <sanbonai0728@gmail.com>
2023-10-15 21:59:40 +0900

    fix: 페이지 이동 시 상태 초기화

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import Header from './components/UI/Header';
 import './App.css';
 import GlobalStyle from './style/GlobalStyle';
 import InfinityScrollText from './components/InfinityScroll/InfinityScrollText';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [isTeamChanged, setIsTeamChanged] = useState(false);
-
   const urlLocation = useLocation();
+
+  useEffect(() => {
+    setIsTeamChanged(false);
+  }, [urlLocation.pathname]);
+
   return (
     <>
       <GlobalStyle />
