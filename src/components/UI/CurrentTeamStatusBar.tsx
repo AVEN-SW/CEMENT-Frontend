216b4ff6e019f4bf2df9b087c9ffecf5b4fc1fdb
kimcurly <sanbonai0728@gmail.com>
2023-11-17 22:31:45 +0900

    fix: 팀 전환 버튼 -> 현재 팀 상태 바 Rename

--- src/components/UI/CurrentTeamStatusBar.tsx
+++ src/components/UI/CurrentTeamStatusBar.tsx
@@ -4,29 +4,37 @@ import styled from 'styled-components';
 interface Props {
   isTeamChanged: boolean;
   setIsTeamChanged: Dispatch<SetStateAction<boolean>>;
+  left?: boolean;
 }
 
-const TeamChangeButton = ({ isTeamChanged, setIsTeamChanged }: Props) => {
+const CurrentTeamStatusBar = ({
+  isTeamChanged,
+  setIsTeamChanged,
+  left,
+}: Props) => {
   return (
     <ButtonContainer
       $team={isTeamChanged}
+      $left={left}
       onClick={() => setIsTeamChanged((prev) => !prev)}
     />
   );
 };
 
-export default TeamChangeButton;
+export default CurrentTeamStatusBar;
 
 const ButtonContainer = styled.button<{
   $team: boolean;
+  $left?: boolean;
 }>`
   width: 125px;
   height: 6px;
 
   border: unset;
-  background: ${(props) =>
-    props.$team
-      ? '#d9d9d9'
-      : 'linear-gradient(270deg, #6A82FB 0%, #FC5C7D 100%)'};
+  background: ${(props) => {
+    if (props.$team && !props.$left) return '#6A82FB';
+    else if (!props.$team && props.$left) return '#FC5C7D';
+    else return '#D4D4D4';
+  }};
   transition: background 0.4s ease;
 `;
