3d2cce8b98b56c3fc8858cf4938efda60d82e824
kimcurly <sanbonai0728@gmail.com>
2023-11-20 23:19:18 +0900

    feat: Current Team Status Bar Add Aven

--- src/components/UI/CurrentTeamStatusBar.tsx
+++ src/components/UI/CurrentTeamStatusBar.tsx
@@ -2,22 +2,28 @@ import styled from 'styled-components';
 
 interface Props {
   isTeamChanged: boolean;
+  team: boolean;
 }
 
-const CurrentTeamStatusBar = ({ isTeamChanged }: Props) => {
-  return <ButtonContainer $team={isTeamChanged} />;
+const CurrentTeamStatusBar = ({ isTeamChanged, team }: Props) => {
+  return <ButtonContainer $isTeamChanged={isTeamChanged} $team={team} />;
 };
 
 export default CurrentTeamStatusBar;
 
 const ButtonContainer = styled.button<{
+  $isTeamChanged: boolean;
   $team: boolean;
 }>`
   position: relative;
 
   width: 250px;
   height: 6px;
-  background-color: #d4d4d4;
+  background-color: ${(props) => {
+    if (props.$team && !props.$isTeamChanged) return '#787878';
+    else if (props.$team && props.$isTeamChanged) return '#383838';
+    else '#D4D4D4';
+  }};
 
   border: unset;
 
@@ -28,13 +34,15 @@ const ButtonContainer = styled.button<{
     width: 125px;
     height: 6px;
 
-    left: ${(props) => (props.$team ? '125px' : '0')};
+    left: ${(props) => (props.$isTeamChanged ? '125px' : '0')};
 
     background: ${(props) => {
-      if (props.$team) return '#6A82FB';
+      if (props.$team && !props.$isTeamChanged) return '#2B99FF';
+      else if (props.$team && props.$isTeamChanged) return '#fff';
+      else if (!props.$team && props.$isTeamChanged) return '#6A82FB';
       else return '#FC5C7D';
     }};
-
     transition: all 0.7s ease-in-out;
   }
+  transition: all 0.7s ease-in-out;
 `;
